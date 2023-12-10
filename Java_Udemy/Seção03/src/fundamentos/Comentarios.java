c522fd2da19cddc81e6c2f7ea54133e01f0eb450
jeperico <jeperico@terra.com.br>
2023-12-10 02:43:44 -0300

    Comentarios

---
+++ Java_Udemy/Seção03/src/fundamentos/Comentarios.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+public class Comentarios {
+
+	/**
+	 * 
+	 * @author João Périco
+	 * @since JDK 1.0
+	 * 
+	 */
+
+	public static void main(String[] args) { // Asi
+		System.out.println("Hello World!");
+
+		// Normal
+		/*
+		 * Linhas
+		 * 
+		 * Linhas
+		 */
+	}
+}
