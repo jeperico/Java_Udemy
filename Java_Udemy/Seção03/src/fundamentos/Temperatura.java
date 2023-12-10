df700ae8530a1c13fa9957c3d77ccb2e6cdb47ea
jeperico <jeperico@terra.com.br>
2023-12-10 16:17:42 -0300

    Temperatura

---
+++ Java_Udemy/Seção03/src/fundamentos/Temperatura.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class Temperatura {
+
+	public static void main(String[] args) {
+		double f = 120.0;
+		final int AJUSTE = 32;
+		final double FATOR = 5.0/9.0;
+		double c = (f - AJUSTE) * FATOR;
+		System.out.println(f + " convertidos para " + c);
+	}
+}
