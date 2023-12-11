2e9f8d1d7801dc65b11bab444460afa18c2ad4a5
jeperico <jeperico@terra.com.br>
2023-12-11 18:02:56 -0300

    Import

---
+++ Java_Udemy/Seção03/src/fundamentos/Import.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+import java.util.Date;
+
+import javax.swing.JButton;
+
+public class Import {
+
+	public static void main(String[] args) {
+		
+		java.lang.String b = "Boa tarde";
+		System.out.println(b);
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+		JButton botao = new JButton();
+	}
+}
