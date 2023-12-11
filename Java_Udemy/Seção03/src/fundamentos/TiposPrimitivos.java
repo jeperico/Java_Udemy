e4785288b53d7278b66ef4f1b74d3af8237a617c
jeperico <jeperico@terra.com.br>
2023-12-11 17:42:55 -0300

    Tipos Primitivos

--- Java_Udemy/Seção03/src/fundamentos/TiposPrimitivos.java
+++ Java_Udemy/Seção03/src/fundamentos/TiposPrimitivos.java
@@ -2,4 +2,32 @@ package fundamentos;
 
 public class TiposPrimitivos {
 
+	public static void main(String[] args) {
+		
+	}
 }
+
+/* Tipos Numérico:
+ * 
+ * 
+ * Inteiros:
+ * 
+ * Byte		1		-128 até +127
+ * Short	2		+- 32.767
+ * Int		4		+- 2 milhôes
+ * Long		8		muito
+ * 
+ * 
+ * Reais:
+ * 
+ * Float	4
+ * Double	8
+ * 
+ */
+
+/* Outros Tipos:
+ * 
+ * Char			String
+ * Boolean		Binário
+ *  
+ */
---
+++ Java_Udemy/Seção03/src/fundamentos/TiposPrimitivosEx.java
@@ -0,0 +1,40 @@
+package fundamentos;
+
+public class TiposPrimitivosEx {
+
+	public static void main(String[] args) {
+		// Informações do funcionário
+		
+		byte anosDeEmpresa = 23;
+		short numeroDeVoos = 542;
+		int id = 567_899; // Dá para separar um número com _ para melhor vizualização
+		long pontosAcumulados = 9_123_745_456L; // L ou l após um número que excede a capacidade dos inteiros
+		
+		float salario = 13_325.23F; // F ou f
+		double vendasAcumuladas = 2_123_563_723.12;
+		
+		
+		boolean estaDeFerias = false;
+		char status = 'A';		
+		
+		// Dias de empresa
+		System.out.println(anosDeEmpresa * 365);
+		
+		// Número de viagens
+		System.out.println(numeroDeVoos / 2);
+		
+		// Pontos por real
+		System.out.println(pontosAcumulados / vendasAcumuladas);
+		
+		// Condição
+		System.out.println("Usuário de ID: " + id + " recebe R$" + salario + " por mês.");
+		
+		
+		if (estaDeFerias) {
+			System.out.println("Usuário de ID: " + id + "está sobre status " + status + " e está férias");
+		} else {
+			System.out.println("Usuário de ID: " + id + "está sobre status " + status + " e não está férias");			
+		}
+		
+	}
+}
