970a348167ea1998898b252649b2299e12af481e
jeperico <jeperico@terra.com.br>
2023-12-10 21:28:01 -0300

    Inferencia

---
+++ Java_Udemy/Seção03/src/fundamentos/Inferencia.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class Inferencia {
+
+	public static void main(String[] args) {
+		
+		double a = 4.5;
+		System.out.println(a);
+		
+		var b = 4.5;
+		System.out.println(b);
+		
+		var c = "Text";
+		System.out.println(c);
+		c = "Other Text";
+		System.out.println(c);
+	}
+}
