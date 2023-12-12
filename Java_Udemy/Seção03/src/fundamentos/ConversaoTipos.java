2a5455ad32c2cbba9dcb098024daac14c446a7bd
jeperico <jeperico@terra.com.br>
2023-12-12 09:58:15 -0300

    Conversão de Tipos

---
+++ Java_Udemy/Seção03/src/fundamentos/ConversaoTipos.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+public class ConversaoTipos {
+
+	public static void main(String[] args) {
+		
+		double a = 1;				// Implícita
+		System.out.println(a);
+		
+		float b = (float) 1.0;		// Explícita (CAST)
+		System.out.println(b);
+
+		int c = 4;					// Explícita (CAST)
+		byte d = (byte) c;
+		
+		double e = 1.9999999999;	// Explícita (CAST)
+		int f = (int) e;
+		System.out.println(d + f);
+	}
+}
