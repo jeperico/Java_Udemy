ca68ac3c6bb7788455b6d3ccc292becd8c0a264e
jeperico <jeperico@terra.com.br>
2023-12-14 13:03:46 -0300

    Desafio Dia da Semana

---
+++ Seção04/src/desafios/DesafioDiaSemana.java
@@ -0,0 +1,36 @@
+package desafios;
+
+import java.util.Scanner;
+
+public class DesafioDiaSemana {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("Informe o dia da semana:");
+		String diaSemana = entrada.next();		
+
+		{
+			if ("domingo".equals(diaSemana.toLowerCase())) {
+				System.out.println(1);
+			} else if ("segunda".equals(diaSemana.toLowerCase())) {
+				System.out.println(2);
+			} else if (("terça".equals(diaSemana.toLowerCase())) || ("terca".equals(diaSemana.toLowerCase()))) {
+				System.out.println(3);
+			} else if ("quarta".equals(diaSemana.toLowerCase())) {
+				System.out.println(4);
+			} else if ("quinta".equals(diaSemana.toLowerCase())) {
+				System.out.println(5);
+			} else if ("sexta".equals(diaSemana.toLowerCase())) {
+				System.out.println(6);
+			} else if ("sabado".equals(diaSemana.toLowerCase())) {
+				System.out.println(7);
+			} else {
+				System.out.println("Dia inválido");
+			}
+		}
+
+		entrada.close();
+	}
+}
