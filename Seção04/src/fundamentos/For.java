c93de2826ebdc808a556ebb42cffe2c0e088b9b9
jeperico <jeperico@terra.com.br>
2023-12-14 13:38:02 -0300

    While and For

---
+++ Seção04/src/fundamentos/For.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class For {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <=20; i++) {
+			System.out.println(i);
+		}
+	}
+}
---
+++ Seção04/src/fundamentos/While.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class While {
+
+	public static void main(String[] args) {
+		
+		int x = 1;
+		
+		while (x <= 10) {
+			System.out.println(x);
+			x++;
+		}
+	}
+}
