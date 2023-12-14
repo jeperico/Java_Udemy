7f6f4bab7dc0b12b7c4c4a82cfa75b37fc7cd14a
jeperico <jeperico@terra.com.br>
2023-12-14 13:07:02 -0300

    Desafio diaSemana correção

--- Seção04/src/desafios/DesafioDiaSemana.java
+++ Seção04/src/desafios/DesafioDiaSemana.java
@@ -12,24 +12,44 @@ public class DesafioDiaSemana {
 		String diaSemana = entrada.next();		
 
 		{
-			if ("domingo".equals(diaSemana.toLowerCase())) {
+			if ("domingo".equalsIgnoreCase(diaSemana)) {
 				System.out.println(1);
-			} else if ("segunda".equals(diaSemana.toLowerCase())) {
+			} else if ("segunda".equalsIgnoreCase(diaSemana)) {
 				System.out.println(2);
-			} else if (("terça".equals(diaSemana.toLowerCase())) || ("terca".equals(diaSemana.toLowerCase()))) {
+			} else if (("terça".equalsIgnoreCase(diaSemana)) || ("terca".equalsIgnoreCase(diaSemana))) {
 				System.out.println(3);
-			} else if ("quarta".equals(diaSemana.toLowerCase())) {
+			} else if ("quarta".equalsIgnoreCase(diaSemana)) {
 				System.out.println(4);
-			} else if ("quinta".equals(diaSemana.toLowerCase())) {
+			} else if ("quinta".equalsIgnoreCase(diaSemana)) {
 				System.out.println(5);
-			} else if ("sexta".equals(diaSemana.toLowerCase())) {
+			} else if ("sexta".equalsIgnoreCase(diaSemana)) {
 				System.out.println(6);
-			} else if ("sabado".equals(diaSemana.toLowerCase())) {
+			} else if ("sabado".equalsIgnoreCase(diaSemana)) {
 				System.out.println(7);
 			} else {
 				System.out.println("Dia inválido");
 			}
 		}
+		
+//		{
+//			if ("domingo".equals(diaSemana.toLowerCase())) {
+//				System.out.println(1);
+//			} else if ("segunda".equals(diaSemana.toLowerCase())) {
+//				System.out.println(2);
+//			} else if (("terça".equals(diaSemana.toLowerCase())) || ("terca".equals(diaSemana.toLowerCase()))) {
+//				System.out.println(3);
+//			} else if ("quarta".equals(diaSemana.toLowerCase())) {
+//				System.out.println(4);
+//			} else if ("quinta".equals(diaSemana.toLowerCase())) {
+//				System.out.println(5);
+//			} else if ("sexta".equals(diaSemana.toLowerCase())) {
+//				System.out.println(6);
+//			} else if ("sabado".equals(diaSemana.toLowerCase())) {
+//				System.out.println(7);
+//			} else {
+//				System.out.println("Dia inválido");
+//			}
+//		}
 
 		entrada.close();
 	}
