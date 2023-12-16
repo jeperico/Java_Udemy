3235d9cc6d0f7a02d32b08c6b77b490ef655b410
jeperico <jeperico@terra.com.br>
2023-12-16 00:03:16 -0300

    Breaks

---
+++ Seção04/src/fundamentos/Break.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class Break {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 10; i++) {
+			
+			System.out.println(i);
+			
+			if (i == 6) break;
+		}
+		System.out.println("Fim");
+	}
+}
