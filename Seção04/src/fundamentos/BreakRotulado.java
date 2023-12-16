3459427e83af90e2550381050b8ed5e9bda68648
jeperico <jeperico@terra.com.br>
2023-12-16 00:04:29 -0300

    Break

--- Seção04/src/fundamentos/BreakRotulado.java
+++ Seção04/src/fundamentos/BreakRotulado.java
@@ -4,6 +4,13 @@ public class BreakRotulado {
 
 	public static void main(String[] args) {
 		
-		
+		externo: for (int i = 0; i < 10; i++) {
+			interno: for (int j = 0; j < 10; j++) {
+				
+				if(i == 1) break externo;
+				
+				if(i == 2) break interno;
+			}
+		}
 	}
 }
