87aa70c2b864276bf531488fc4d774f51bd05ebe
jeperico <jeperico@terra.com.br>
2023-12-23 10:51:42 -0300

    Static (Desconto)

--- Seção05/src/fundamentos/ProdutoTeste.java
+++ Seção05/src/fundamentos/ProdutoTeste.java
@@ -4,17 +4,16 @@ public class ProdutoTeste {
 
 	public static void main(String[] args) {
 		
-		Produto p1 = new Produto("Celular", 4567.23, 0.2);
+		Produto p1 = new Produto("Celular", 4567.23);
 		var p2 = new Produto();
 		
 		p2.nome = "Note";
 		p2.preco = 8765.43;
-		p2.desconto = 0.15;
 		
 		System.out.println(p1);
 		System.out.println(p1.nome.toUpperCase());
 		System.out.println(p2.preco);
 		System.out.println(p2.precoDescontado());
-		System.out.println(p2.preco * (1 - p2.desconto));
+		System.out.println(p2.preco * (1 - Produto.desconto));
 	}
 }
