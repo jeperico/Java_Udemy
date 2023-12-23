315a0c4e942f0dff68bd4b6fbfb8c8fac31a555c
jeperico <jeperico@terra.com.br>
2023-12-23 11:14:15 -0300

    Valor x Referencia

---
+++ Seção05/src/desafios/ValorVsReferencia.java
@@ -0,0 +1,37 @@
+package desafios;
+
+import fundamentos.Produto;
+
+public class ValorVsReferencia {
+
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = a;
+		
+		a++;
+		b--;
+		
+		System.out.println(a + " " + b);
+		
+		Data d1 = new Data();
+		Data d2 = d1;
+		
+		d1.dia = 7;
+		d2.mes = 2;		
+		
+		System.out.println(d1.dataFormatada());
+		System.out.println(d2.dataFormatada());
+		
+		returnData(d2);
+		
+		System.out.println(d1.dataFormatada());
+		System.out.println(d2.dataFormatada());
+	}
+	
+	static void returnData(Data d) {
+		d.dia = 1;
+		d.mes = 1;
+		d.ano = 1970;
+	}
+}
