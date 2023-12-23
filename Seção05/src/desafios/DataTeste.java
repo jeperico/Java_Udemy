ed8dd954668a6a6ad0cd5f7f56e5b169e3689a9e
jeperico <jeperico@terra.com.br>
2023-12-22 23:13:58 -0300

    Construtores (Data)

--- Seção05/src/desafios/DataTeste.java
+++ Seção05/src/desafios/DataTeste.java
@@ -4,24 +4,14 @@ public class DataTeste {
 
 	public static void main(String[] args) {
 		
-		var data01 = new Data();
+		var data01 = new Data(22, 12, 2023);
 		var data02 = new Data();
-		var data03 = new Data();
-		
-		data01.dia = 22;
-		data01.mes = 12;
-		data01.ano = 2023;
-		
-		data02.dia = 1;
-		data02.mes = 1;
-		data02.ano = 2001;
-		
-		data03.dia = 12;
-		data03.mes = 12;
-		data03.ano = 2012;
+		var data03 = new Data(12, 12, 2012);
+		var data04 = new Data(7, 3, 2012);
 		
 		System.out.printf("Data atual: %s.\n", data01.dataFormatada());
 		System.out.println(data02.dataFormatada());
 		System.out.println(data03.dataFormatada());		
+		System.out.println(data04.dataFormatada());		
 	}
 }
