fafff751366f599193dd295f84e4d4c65da5f815
jeperico <jeperico@terra.com.br>
2023-12-23 11:34:52 -0300

    This and This() (Data)

--- Seção05/src/desafios/Data.java
+++ Seção05/src/desafios/Data.java
@@ -7,15 +7,13 @@ public class Data {
 	int ano;
 	
 	Data () {
-		dia = 1;
-		mes = 1;
-		ano = 1970;
+		this(1, 1, 1970); // Referencia outro construtor
 	}
 	
-	Data (int diaInicial, int mesInicial, int anoInicial) {
-		dia = diaInicial;
-		mes = mesInicial;
-		ano = anoInicial;
+	Data (int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
 	}
 	
 	String dataFormatada() {
