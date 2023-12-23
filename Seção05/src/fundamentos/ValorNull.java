371c603bf81d4bbea5a1a6f4f0e750bae36bd1e9
jeperico <jeperico@terra.com.br>
2023-12-23 12:02:10 -0300

    Null and Try/Catch

---
+++ Seção05/src/fundamentos/ValorNull.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+public class ValorNull {
+
+	public static void main(String[] args) {
+		
+		try {
+			;
+//			String s1 = null;
+//			System.out.println(s1.concat("!!!!!!"));
+			System.out.println("Bala");
+			
+		} catch (Exception e) {
+			System.out.println("Deu erro kkkk.");
+			System.out.println(e);
+		}
+	}
+}
