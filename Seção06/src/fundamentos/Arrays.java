998de514c6f1c60f976af39bd90672360c43baa2
jeperico <jeperico@terra.com.br>
2023-12-23 14:08:22 -0300

    Seção06 Arrays
    Mano, Array em Java é horrível, igual Pascal / Portugol.

---
+++ Seção06/src/fundamentos/Arrays.java
@@ -0,0 +1,25 @@
+package fundamentos;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		int [] a = new int[6];
+		a[2] = 100;
+		a[0] = 9;
+		
+		System.out.println(java.util.Arrays.toString(a));
+		
+//		double[][] arrayArray = new double[3][3];
+		
+		for(int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+		System.out.println();
+
+		double [] notas = {5.7, 8.9, 3.7, 10, 9.8 };
+		for (int i = 0; i < notas.length; i++) {
+			System.out.println(notas[i]);			
+		}
+	}
+}
