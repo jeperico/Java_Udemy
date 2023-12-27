9aaf300fd0436353b7a4fb6b69cb4f1e3ad9e70a
jeperico <jeperico@terra.com.br>
2023-12-27 12:12:27 -0300

    Set

---
+++ Seção06/src/fundamentos/Set.java
@@ -0,0 +1,12 @@
+package fundamentos;
+
+public class Set {
+
+	//	Pode ser heterogêneo	(Ruim)
+	//	Pode ser homogêneo	(Boa Prática)
+	//	Não aceita dados duplicados
+	//	Pode ser duplicado
+	//	Não é indexado
+	
+	
+}
