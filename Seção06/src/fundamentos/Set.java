2766994fe4a33ca97672ac157fcb2bd962bd8c7d
jeperico <jeperico@terra.com.br>
2024-01-03 01:59:10 -0300

    Set

--- Seção06/src/fundamentos/Set.java
+++ Seção06/src/fundamentos/Set.java
@@ -1,5 +1,7 @@
 package fundamentos;
 
+import java.util.HashSet;
+
 public class Set {
 
 	//	Pode ser heterogêneo	(Ruim)
@@ -8,5 +10,39 @@ public class Set {
 	//	Pode ser duplicado
 	//	Não é indexado
 	
-	
+	public static void main(String[] args) {
+		
+		HashSet<E> conjunto = new HashSet<E>();
+		
+		conjunto.add(1.2); // double -> Double
+		conjunto.add(true);
+		conjunto.add("Texte");
+		conjunto.add(1);
+		conjunto.add('x');
+		
+		System.out.println(conjunto.size());
+		System.out.println(conjunto.remove(true));
+		System.out.println(conjunto.size());
+		System.out.println(conjunto.contains(1.2));
+		System.out.println(conjunto.contains(1.3));
+		
+		java.util.Set<E> num = new HashSet<E>();
+		num.add(1);
+		num.add(2);
+		num.add(3);
+		System.out.println(num);
+		System.out.println(conjunto);
+		
+		conjunto.addAll(num);
+		System.out.println(num);
+		System.out.println(conjunto);
+		
+		conjunto.retainAll(num);
+		System.out.println(conjunto);
+		
+		
+		conjunto.clear();
+		System.out.println(conjunto);
+		
+	}
 }
