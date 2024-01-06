2162950e5ba302deb319fa8efbee3aca526e733c
jeperico <jeperico@terra.com.br>
2024-01-06 16:46:41 -0300

    Null

--- Seção05/src/fundamentos/ValorNull.java
+++ Seção05/src/fundamentos/ValorNull.java
@@ -2,12 +2,13 @@ package fundamentos;
 
 public class ValorNull {
 
+	@SuppressWarnings("null")
 	public static void main(String[] args) {
 		
 		try {
 			;
-//			String s1 = null;
-//			System.out.println(s1.concat("!!!!!!"));
+			String s1 = null;
+			System.out.println(s1.concat("!!!!!!"));
 			System.out.println("Bala");
 			
 		} catch (Exception e) {
