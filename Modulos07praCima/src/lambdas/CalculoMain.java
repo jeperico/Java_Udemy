efe18efe375c1c3e4c547451b690762d6a892686
jeperico <jeperico@terra.com.br>
2024-01-07 12:44:47 -0300

    First Lambda Function
    Carinha de JavaScript, delicia

---
+++ Modulos07praCima/src/lambdas/CalculoMain.java
@@ -0,0 +1,11 @@
+package lambdas;
+
+public class CalculoMain {
+
+	public static void main(String[] args) {
+		
+		Calculo soma = new Soma();
+		System.out.println(soma.executar(2, 3));
+		
+	}
+}
---
+++ Modulos07praCima/src/lambdas/CalculoMain2.java
@@ -0,0 +1,14 @@
+package lambdas;
+
+public class CalculoMain2 {
+
+	public static void main(String[] args) {
+		
+		Calculo calculo = (x, y) -> { return x + y; };
+		System.out.println(calculo.executar(2, 4));
+		
+		calculo = (x, y) -> x * y;
+		System.out.println(calculo.executar(2, 4));
+		
+	}
+}
